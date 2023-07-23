1f68a0bda8eb83bc3c375230c815de2c9db4df20
Rodel L. Aspa <rlaspa@ENTDSWD.LOCAL>
2023-07-23 13:23:08 +0800

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainHeaderComponent } from './shared/components/main-header/main-header.component';
+import { MainSidebarComponent } from './shared/components/main-sidebar/main-sidebar.component';
+import { ContentWrapperComponent } from './shared/components/content-wrapper/content-wrapper.component';
+import { ControlSidebarComponent } from './shared/components/control-sidebar/control-sidebar.component';
+import { MainFooterComponent } from './shared/components/main-footer/main-footer.component';
+import { PreLoaderComponent } from './shared/components/pre-loader/pre-loader.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainHeaderComponent,
+    MainSidebarComponent,
+    ContentWrapperComponent,
+    ControlSidebarComponent,
+    MainFooterComponent,
+    PreLoaderComponent
   ],
   imports: [
     BrowserModule,
